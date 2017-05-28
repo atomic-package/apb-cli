d17b96cf110c7a51b1553b6fe19975aeabdfa9a3
panicdragon <d.takayama.web@gmail.com>
2017-05-29 03:23:43 +0900

    add model files

--- packages/commands/new.ts
+++ packages/commands/new.ts
@@ -1,18 +1,19 @@
-const fs = require('fs');
+import App from  '../config/app';
 
+const fs = require('fs');
 
 export class New {
   constructor(params) {
     console.log('new', params);
 
-    fs.mkdir(params.directoryPath, '757', () => {
+    fs.mkdir(params.directoryPath, App.DIRECTORY_PERMISSION, () => {
       let data            = "//test";
       let common_inc_file = params.directoryPath + '/_common_inc.scss';
       let parts_file      = params.directoryPath + '/_parts.scss';
       let style_file      = params.directoryPath + '/style.scss';
 
 
-      fs.mkdir(params.baseDirectoryPath, '757', () => {
+      fs.mkdir(params.baseDirectoryPath, App.DIRECTORY_PERMISSION, () => {
         let base_file    = params.baseDirectoryPath + '/_base.scss';
         let mixin_file   = params.baseDirectoryPath + '/_mixin.scss';
         let reset_file   = params.baseDirectoryPath + '/_reset.scss';
@@ -37,95 +38,95 @@ export class New {
       });
 
 
-      fs.mkdir(params.pagesDirectoryPath, '757', () => {
+      fs.mkdir(params.pagesDirectoryPath, App.DIRECTORY_PERMISSION, () => {
         let pages_file = params.pagesDirectoryPath + '/_pages_inc.scss';
         let index_file = params.pagesDirectoryPath + '/_index.scss';
 
-        fs.writeFile(pages_file, data, function(err) {
+        fs.writeFile(pages_file, data, (err) => {
           if (err) { throw err; }
         });
 
-        fs.writeFile(index_file, data, function(err) {
-          if (err) { throw err; }
-        });
-      });
-
-
-      fs.mkdir(params.partsDirectoryPath, '757', function () {
-        fs.writeFile(params.partsDirectoryPath + '/_animation.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_base.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_bg.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_box.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_button.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_footer.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_header.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_icon.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_list.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_media_queries.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_modal.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_navigation_menu.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_paragraph.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_separate.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_table.scss', data, function(err) {
-          if (err) { throw err; }
-        });
-
-        fs.writeFile(params.partsDirectoryPath + '/_title.scss', data, function(err) {
+        fs.writeFile(index_file, data, (err) => {
           if (err) { throw err; }
         });
       });
 
-      fs.writeFile(common_inc_file, data, function(err) {
+
+      fs.mkdir(params.partsDirectoryPath, App.DIRECTORY_PERMISSION, () => {
+        fs.writeFile(params.partsDirectoryPath + '/_animation.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_base.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_bg.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_box.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_button.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_footer.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_header.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_icon.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_list.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_media_queries.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_modal.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_navigation_menu.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_paragraph.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_separate.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_table.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+
+        fs.writeFile(params.partsDirectoryPath + '/_title.scss', data, (err) => {
+          if (err) { throw err; }
+        });
+      });
+
+      fs.writeFile(common_inc_file, data, (err) => {
         if (err) { throw err; }
       });
 
-      fs.writeFile(parts_file, data, function(err) {
+      fs.writeFile(parts_file, data, (err) => {
         if (err) { throw err; }
       });
 
-      fs.writeFile(style_file, data, function(err) {
+      fs.writeFile(style_file, data, (err) => {
         if (err) { throw err; }
       });
 
--- packages/config/app.ts
+++ packages/config/app.ts
@@ -1,5 +1,6 @@
 const App = {
-  VERSION: "0.0.1"
+  VERSION: "0.0.1",
+  DIRECTORY_PERMISSION: '757'
 };
 
 export default App;
